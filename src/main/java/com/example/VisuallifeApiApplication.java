ce899c6c05e7989c15c21bd02da707b08da95b0d
luohuan <297750341@qq.com>
2016-10-17 10:52:44 +0800

    项目基本结构上传

---
+++ src/main/java/com/example/VisuallifeApiApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisuallifeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisuallifeApiApplication.class, args);
+	}
+}
