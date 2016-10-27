9c3e44c39f32a01bad0472deb0050f15e1826c71
luohuan <297750341@qq.com>
2016-10-27 14:51:39 +0800

    cros跨域请求支持

--- src/main/java/lh/world/api/filter/SimpleCORSFilter.java
+++ src/main/java/lh/world/api/filter/SimpleCORSFilter.java
@@ -20,6 +20,7 @@ public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) response;
         res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
     }
 
     @Override
