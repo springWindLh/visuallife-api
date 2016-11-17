17fb5f45722676416fe338fa1b8ee44a18fcd35b
luohuan <297750341@qq.com>
2016-11-17 16:47:04 +0800

    增加注册方法

--- src/main/java/lh/world/api/controller/LoginController.java
+++ src/main/java/lh/world/api/controller/LoginController.java
@@ -5,8 +5,11 @@ import lh.world.api.controller.support.AjaxResponse;
 import lh.world.api.controller.support.BaseController;
 import lh.world.api.form.LoginForm;
 import lh.world.base.domain.User;
+import lh.world.base.form.UserForm;
 import lh.world.base.service.UserService;
+import lh.world.base.util.EncrptUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,6 +39,22 @@ public class LoginController extends BaseController {
             return AjaxResponse.ok().msg("登录成功").data(userOptional.get());
         } else {
             return AjaxResponse.fail().msg(errorMsg);
+        }
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public AjaxResponse register(@RequestBody UserForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return getErrorInfo(result);
+        }
+        User user = form.asUser();
+        user.setPassword(EncrptUtil.encodePassword(user.getPassword()));
+        user.setRole(User.Role.USER);
+        try {
+             User currentUser = userService.save(user);
+            return AjaxResponse.ok().msg("注册成功").data(currentUser);
+        } catch (RuntimeException e) {
+            return AjaxResponse.fail().msg(e.getMessage());
         }
     }
 }
